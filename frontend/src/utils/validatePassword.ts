72a396e47d7c7e1b68686840415a52bb93c711f0
JoaoPedroSimsic <joao.simsic@4x4brasil.com.br>
2025-08-31 18:53:55 -0300

    fix: added limit to validate password logic

--- frontend/src/utils/validatePassword.ts
+++ frontend/src/utils/validatePassword.ts
@@ -1,6 +1,6 @@
 const validatePassword = (password: string): string | null => {
   if (!password) return "Password is required";
-  if (password.length < 6) return "Password must be at least 6 characters";
+  if (password.length < 6 || password.length > 30) return "Password must be 6 to 30 charaters long";
   return null;
 };
 
