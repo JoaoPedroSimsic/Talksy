18ac0f86582b970cf3df0ba97cc8fc6db94ffdee
JoaoPedroSimsic <joao.simsic@4x4brasil.com.br>
2025-08-31 18:52:53 -0300

    chore: updated the error handling function

--- frontend/src/utils/handleError.ts
+++ frontend/src/utils/handleError.ts
@@ -2,13 +2,23 @@ import React from 'react';
 
 function handleError(
 	err: unknown,
-	setError: React.Dispatch<React.SetStateAction<string | null>>,
-) {
+	setError: React.Dispatch<React.SetStateAction<string>>,
+): void {
+	if (typeof err === 'object' && err && 'response' in err) {
+		const axiosErr = err as { response?: { data?: { error?: string } } };
+
+		if (axiosErr.response?.data?.error) {
+			setError(axiosErr.response.data.error);
+			return;
+		}
+	}
+
 	if (err instanceof Error) {
 		setError(err.message);
-	} else {
-		setError('An unknown error occurried');
+		return;
 	}
+
+	setError('An unknown error occurred');
 }
 
 export default handleError;
