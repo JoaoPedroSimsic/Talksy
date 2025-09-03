87dcddf4181138a33437a4df528bd587cf08bfbb
JoaoPedroSimsic <joao.simsic@4x4brasil.com.br>
2025-09-02 22:44:47 -0300

    draft: profile page

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
+
+interface User {
+  username: string;
+  email: string;
+  photo: string | null;
+}
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+const userCheckEndpoint = `${backendUrl}/users/me`;
+
+const fetchUser = async (): Promise<void> => {
+  const response = axios.get<User>(userCheckEndpoint, {
+		withCredentials: true,
+	});
+};
 
 const Profile: React.FC = (): React.ReactNode => {
-	return (
-		<div>
-			<p>profile</p>
-		</div>
-	);
+  return (
+    <div className='px-5 h-full w-full'>
+      <div className='h-full'></div>
+    </div>
+  );
 };
 
 export default Profile;
