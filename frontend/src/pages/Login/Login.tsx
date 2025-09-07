7241c603ae0ced127d3b95059c9a5271071d41e7
JoaoPedroSimsic <joao.simsic@4x4brasil.com.br>
2025-09-07 19:35:01 -0300

    refac: removing old background

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -7,18 +7,13 @@ import Logo from '../../components/Logo';
 const Login: React.FC = (): React.ReactNode => {
 	return (
 		<div className='flex w-full items-center justify-between h-screen'>
-			<div className='bg-[var(--primary)] desktop:bg-[var(--bg-dark)] relative w-full desktop:w-1/2 h-screen flex items-center justify-center overflow-hidden'>
+			<div className='bg-[var(--bg-dark)] desktop:bg-[var(--bg-dark)] relative w-full desktop:w-1/2 h-screen flex items-center justify-center overflow-hidden'>
 				<div className='hidden z-1 desktop:flex absolute top-10 left-10 h-10 w-40'>
 					<Logo />
 					<span className='flex items-center justify-center ml-1 text-[var(--text)] text-xl font-bold'>
 						Talksy
 					</span>
 				</div>
-				<img
-					src='/assets/background.svg'
-					className='desktop:hidden absolute scale-150'
-					alt='background'
-				/>
 				<LoginForm />
 			</div>
 			<div className='relative h-full w-1/2 bg-[var(--bg)] hidden desktop:flex flex-col justify-end overflow-hidden shadow-lg'>
