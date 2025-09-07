e3120d19a2914084ce807ea2d15689bd323b2ac4
JoaoPedroSimsic <joao.simsic@4x4brasil.com.br>
2025-09-07 19:40:48 -0300

    refac: changing logo in register to component

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -7,12 +7,7 @@ import Logo from '../../components/Logo';
 const Register: React.FC = (): React.ReactNode => {
   return (
     <div className='flex w-full items-center justify-between h-screen'>
-      <div className='bg-[var(--primary)] desktop:bg-[var(--bg-dark)] relative w-full desktop:w-1/2 h-screen flex items-center justify-center overflow-hidden'>
-        <img
-          src='/assets/background.svg'
-          className='desktop:hidden absolute scale-150'
-          alt='background'
-        />
+      <div className='bg-[var(--bg-dark)] desktop:bg-[var(--bg-dark)] relative w-full desktop:w-1/2 h-screen flex items-center justify-center overflow-hidden'>
         <div className='hidden desktop:flex z-1 absolute top-10 left-10 h-10 w-40'>
 					<Logo />
           <span className='flex items-center justify-center ml-1 text-[var(--text)] text-xl font-bold'>
@@ -21,7 +16,7 @@ const Register: React.FC = (): React.ReactNode => {
         </div>
         <RegisterForm />
       </div>
-			<div className='relative h-full w-1/2 bg-[var(--bg)] flex flex-col justify-end overflow-hidden shadow-lg'>
+			<div className='relative hidden desktop:flex h-full w-1/2 bg-[var(--bg)] flex-col justify-end overflow-hidden shadow-lg'>
 				<FakeChat />
 			</div>
     </div>
--- frontend/src/pages/Register/components/RegisterForm.tsx
+++ frontend/src/pages/Register/components/RegisterForm.tsx
@@ -4,6 +4,7 @@ import parseAxiosError from '../../../helpers/parseAxiosError';
 import { useNavigate } from 'react-router-dom';
 
 import PasswordInput from '../../../components/PasswordInput';
+import Logo from '../../../components/Logo';
 import { useAuth } from '../../../context/useAuth';
 import validateEmail from '../../../utils/validateEmail';
 import validatePassword from '../../../utils/validatePassword';
@@ -132,7 +133,7 @@ const RegisterForm: React.FC = (): React.ReactNode => {
 	return (
 		<div className='bg-[var(--bg)] relative flex flex-col items-center justify-center max-h-[95%] desktop:h-auto w-4/6 py-10 rounded-xl'>
 			<div className='flex desktop:hidden absolute top-7 left-7 h-10 w-40'>
-				<img src='/assets/logo.svg' className='scale-70' alt='background' />
+				<Logo />
 				<span className='flex items-center justify-center ml-1 text-[var(--text)] text-xl font-bold'>Talksy</span>
 			</div>
 			<div className='flex flex-col space-y-5 items-center justify-center desktop:my-3'>
