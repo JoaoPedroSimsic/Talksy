cc184ca93d3a1598602d44aa08c46632eb2557b7
JoaoPedroSimsic <joao.simsic@4x4brasil.com.br>
2025-09-07 22:26:51 -0300

    chore: better bg and shadow in visibility button

--- frontend/src/components/PasswordInput.tsx
+++ frontend/src/components/PasswordInput.tsx
@@ -31,7 +31,7 @@ const PasswordInput: React.FC<PasswordInputProps> = ({
 				<button
 					type='button'
 					onClick={() => setShowPassword(!showPassword)}
-					className='absolute right-2 flex items-center justify-center cursor-pointer hover:bg-[var(--bg-dark)] p-1 rounded-full transition'
+					className='absolute right-2 flex items-center justify-center cursor-pointer hover:bg-[var(--bg-light)] hover:shadow-2xl p-1 rounded-full transition'
 				>
 					{showPassword ? (
 						<MdOutlineVisibilityOff size={25} className='text-[var(--primary)]' />
