55358313e2c3e17f68860eb4600de2bc8db3e24e
JoaoPedroSimsic <jpsimsic@hotmail.com>
2025-09-21 17:26:25 -0300

    refac: removed header from app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,12 +7,12 @@ import ProtectedRoutes from './components/ProtectedRoutes';
 import LoginRedirect from './pages/Auth/Login/LoginRedirect';
 import RegisterPage from './pages/Auth/Register/Register';
 
-import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar';
+import Sidebar from '@/components/Sidebar';
 
 import HomePage from './pages/Home/Home';
 import ProfilePage from './pages/Profile/Profile';
 import SettingsPage from './pages/Settings/Settings';
+import ChangeEmailPage from '@/pages/Settings/pages/ChangeEmailPage';
 
 const AppContent: React.FC = (): React.ReactNode => {
 	const location = useLocation();
@@ -20,11 +20,6 @@ const AppContent: React.FC = (): React.ReactNode => {
 
 	return (
 		<div className='flex flex-col desktop:flex-row h-screen w-screen bg-[var(--bg)]'>
-			{!hideLayout && (
-				<header className='flex justify-center items-center desktop:hidden w-screen h-1/10'>
-					<Header />
-				</header>
-			)}
 
 			{!hideLayout && (
 				<div className='flex justify-center items-center desktop:flex-col h-15 w-screen desktop:h-screen order-last desktop:order-first px-10'>
@@ -41,6 +36,7 @@ const AppContent: React.FC = (): React.ReactNode => {
 						<Route path='/' element={<HomePage />} />
 						<Route path='/profile' element={<ProfilePage />} />
 						<Route path='/settings' element={<SettingsPage />} />
+						<Route path='change-email' element={<ChangeEmailPage />} />
 					</Route>
 
 					<Route path='*' element={<div>404 Page not found</div>} />
